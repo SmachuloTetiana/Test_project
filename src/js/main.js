0284b1312ef4e00ac9ac447e7d95e3692795fb82
SmachuloTetiana <tanichk@ukr.net>
2019-03-21 10:33:39 +0200

    push changes

--- src/js/main.js
+++ src/js/main.js
@@ -4,23 +4,14 @@
         $(this).parent().find('.header-navbar__nav').slideToggle();
     })
 
-    function wpc_add_img_bg( img_sel, parent_sel){
-		if (!img_sel) {
-		  console.info('no img selector');
-		  return false;
-		}
-		var $parent, _this;
-		$(img_sel).each(function(){
-		  _this = $(this);
-		  $parent = _this.closest( parent_sel );
-		  $parent = $parent.length ? $parent : _this.parent();
-		  $parent.css( 'background-image' , 'url(' + this.src + ')' );
-		  _this.hide()
-		});
-	}
 
-	$(window).on('load', function(){
-		wpc_add_img_bg('.bg-image');
-	});
+    $(window).resize(function() {
+        var width = $(window).width();
+        if(width > 768) {
+            $('.header-navbar__nav').css("display", "flex");
+        } else {
+            $('.header-navbar__nav').css("display", "none");            
+        }
+    })
 
 })(window.jQuery);
